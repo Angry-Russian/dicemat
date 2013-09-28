2097ae5f2530f8ed34b1279cc348f1570f19e469
Dmitri Kolytchev <kolytchev.d@gmail.com>
2013-09-28 00:07:33 -0400

    Added client identification and association

--- src/Dicemat/Chat.php
+++ src/Dicemat/Chat.php
@@ -19,28 +19,40 @@ class Chat implements MessageComponentInterface{
 	}
 
 	public function onMessage(ConnectionInterface  $from, $msg){
-		echo sprintf("Connection %d sending message %s\n", $from->resourceId, $msg);
 
 		$req = json_decode($msg);
+		$sender = $this->clients[$from];
 
-		/*switch($req->type){
-			case "roll": break;
-			case "identify": $referals[$req->id] = array($from); break;
-			case "connect":
-				foreach($req->clients as $ref){
-					$ex = in_array($from, $referals[$ref]);
-					if($referals[$ref] && !$ex) array_push($referals[$ref], $from);
-					else if($ex) echo sprintf("connection %d already exists in $ref", $from->resourceId);
-					else echo sprintf("%d -> %d : requested connection doesn't exist", $from->resourceId, $ref);
+		switch($req->type){
+			case "roll":
+				foreach($this->referals[$sender] as $k=>$cli){
+					if($cli!==$from) $cli->send($msg);
+				}
+			break;
+			case "identify":
+				$oldID = $this->clients[$from];
+				$newID = $req->id;
+
+				if($oldID !== $newID){
+					$this->referals[$newID] = $this->referals[$oldID]?:array($from);
+					$this->clients[$from] = $newID;
+					unset($this->referals[$oldID]);
 				}
 				break;
-			case "remove": break;
-			default:  break;
-		}//*/
 
-		var_dump($from);
-		foreach($this->clients as $cli){
-			if($cli !== $from) $cli->send($msg);
+			case "connect":
+				foreach($req->clents as $c){
+					array_push($this->referals[$c], $from);
+				}
+				break;
+			case "remove":
+				foreach($req->clients as $cli){
+					if(($k = array_search($from, $this->referals[$cli]))!== false){
+						unset($this->referals[$cli][$k]);
+					}
+				}
+				break;
+			default:  break;
 		}
 	}
 
