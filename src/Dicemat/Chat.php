9cef0570e3e5ebc458083277caeb2558979fd40d
Dmitri Kolytchev <kolytchev.d@gmail.com>
2013-10-14 23:36:23 -0400

    Cleanup

--- src/Dicemat/Chat.php
+++ src/Dicemat/Chat.php
@@ -17,7 +17,6 @@ class Chat implements MessageComponentInterface{
 	public function onOpen(ConnectionInterface  $con){
 		$this->clients->attach($con);
 		$this->broadcasters[$con->resourceId] = array("name"=>"", "clients"=>array());
-		//echo "New connection! ({$con->resourceId})\n";
 	}
 
 	public function onMessage(ConnectionInterface  $from, $msg){
@@ -27,9 +26,6 @@ class Chat implements MessageComponentInterface{
 
 		switch($req->type){
 			case "roll":
-				//echo "\"{$sender}\" is rolling:".PHP_EOL;
-				//var_dump($req);
-				//var_dump(json_encode($req));
 				$req->name = $this->broadcasters[$from->resourceId]['name'];
 				$req->id = $from->resourceId;
 				foreach($this->broadcasters[$from->resourceId]['clients'] as $cli){
@@ -39,18 +35,17 @@ class Chat implements MessageComponentInterface{
 			case "identify":
 				$this->broadcasters[$from->resourceId]["name"] = $req->name;
 				foreach($this->broadcasters[$from->resourceId]["clients"] as $cli){
+
 					$cli->send('{"type":"rename", "id":'.$from->resourceId.', "name":"'.$req->name.'"}');
 				};
 
 			break;
 			case "connect":
-				//echo "$sender ({$from->resourceId}) attempting to connect to ";
 				$success = false;
 				$client = null;
 				if($req->name){
 					foreach($this->clients as $cli){
 						if($this->broadcasters[$cli->resourceId]['name'] === $req->name && !in_array($from, $this->broadcasters[$cli->resourceId]['clients'])){
-							//echo $this->broadcasters[$cli->resourceId]['name']. "(".$cli->resourceId.")";
 							array_push($this->broadcasters[$cli->resourceId]['clients'], $from);
 							$success = $cli->send('{"type":"connect", "id":'.$from->resourceId.', "name":"'.$sender.'", "avatar":""}');
 							$client = $cli->resourceId;
@@ -62,7 +57,6 @@ class Chat implements MessageComponentInterface{
 				}else{
 
 				}
-				//echo PHP_EOL;
 			break;
 			case "remove": break;
 			default:  break;
@@ -76,23 +70,19 @@ class Chat implements MessageComponentInterface{
 		foreach($this->clients as $cli){
 			if(($ind = array_search($con, $this->broadcasters[$cli->resourceId]['clients'])) !== false && $cli !== $con){
 				$cli->send('{"type":"leave", "id":'.$con->resourceId.', "name":"'.$sender.'"}');
-				//echo "sending Guest Disconnected signal to ".$cli->resourceId.PHP_EOL;
 			}
 		}
 
 		foreach($this->broadcasters[$con->resourceId]['clients'] as $cli){
 			if($cli!==$con){
 				$cli->send('{"type":"quit", "id":'.$con->resourceId.', "name":"'.$sender.'"}');
-				//echo "sending Host Disconnected signal to ".$cli->resourceId.PHP_EOL;
 			}
 		}
 
         $this->clients->detach($con);
-        //echo "Connection {$con->resourceId} has disconnected\n";
 	}
 
 	public function onError(ConnectionInterface  $con, \Exception $e){
-        //echo "An error has occurred: {$e->getMessage()}\n";
         $con->close();
 	}
 }
